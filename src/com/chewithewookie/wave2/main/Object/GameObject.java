962e6f10c2b978f40f82e72fdb1669af8b4cc946
Colin L <darkblade.ca@gmail.com>
2020-02-19 21:20:00 -0600

    GameObject updated

--- src/com/chewithewookie/wave2/main/Object/GameObject.java
+++ src/com/chewithewookie/wave2/main/Object/GameObject.java
@@ -1,4 +1,8 @@
 package com.chewithewookie.wave2.main.Object;
 
 public class GameObject {
+
+    protected float x, y, velX, velY;
+    protected ID id;
+
 }
--- src/com/chewithewookie/wave2/main/STATE/GameOver.java
+++ src/com/chewithewookie/wave2/main/STATE/GameOver.java
@@ -1,4 +1,9 @@
 package com.chewithewookie.wave2.main.STATE;
 
 public class GameOver extends States {
+
+    public void clicked(int mx, int my) {
+
+    }
+
 }
